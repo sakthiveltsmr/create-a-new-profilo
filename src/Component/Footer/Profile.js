8ad7459cba21a1255c18c30aff8b5a94bc327d55
sakthiveltsmr <sakthiveltsmr@gmail.com>
2021-12-30 00:05:37 +0530

    Profilo

---
+++ src/Component/Footer/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../Home/Home";
+import Footer from "./Footer";
+import "./Profile.css";
+export default function Profile() {
+  return (
+    <div className="home-container">
+      <Home />
+      <Footer />
+    </div>
+  );
+}
