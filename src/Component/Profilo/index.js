4a1ec8cff0f121b1e0945d50923859d750de0a17
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-24 14:16:56 +0530

    index

--- src/Component/Profilo/index.js
+++ src/Component/Profilo/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import "./Profile.css";
+import * as SiIcons from "react-icons/si";
+import blog from "../../img/blog.png";
 export default function Profile() {
-  return <div>Profile</div>;
+  return <div id="portfolio"></div>;
 }
