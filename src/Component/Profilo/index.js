a4a9447d762fbf9290713e4e37ca6cd246a9bc16
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-29 09:21:44 +0530

    index

--- src/Component/Profilo/index.js
+++ src/Component/Profilo/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import "./Profile.css";
 import * as SiIcons from "react-icons/si";
-// import diet from "../../Images/dietapp.png";
-// import gmail from "../../Images/gmailapp.png";
-// import ecom from "../../Images/ecommerceapp.png";
+
+import car from "../../img/car.png";
+import pizza from "../../img/screen.png";
 
 function Portfolio() {
   return (
     <div id="portfolio">
       <h1 className="logo-text">My Projects</h1>
       <div className="allProjects">
-        {/* <div className="card" data-aos="zoom-in-up">
-          <img src={ecom} className="card-img-top" alt="..." />
+        <div className="card" data-aos="zoom-in-up">
+          <img src={pizza} className="card-img-top" alt="..." />
           <div className="title_heading">
-            <h5 className="card-title">E-Commerce Application</h5>
+            <h5 className="card-title">Pizza Application</h5>
           </div>
           <div className="card-img-overlay">
-            <div className="img-title">Shopify</div>
+            <div className="img-title">Pizza Beast</div>
             <div className="img-description">
               <p>
-                User can filter products by Brand,Product_type,Price range using
-                filter bars.{" "}
+                Users can filter available foods using filter option based on
+                the category (Veg or non-veg).{" "}
               </p>
               <p>
                 Implemented this application with complete authentication system
@@ -35,7 +35,7 @@ function Portfolio() {
                     className="bth btn-sm btn-success ml-3"
                     target="_blank"
                     rel="noreferrer"
-                    href="https://github.com/mkandanmk7/React-E-commerce-Client"
+                    href="https://github.com/sakthiveltsmr/React-Pizza-Front-End"
                   >
                     Frontend <i className="fab fa-github"></i>
                   </a>
@@ -43,7 +43,7 @@ function Portfolio() {
                     className="bth btn-sm btn-success ml-3"
                     target="_blank"
                     rel="noreferrer"
-                    href="https://github.com/mkandanmk7/Node-JS-E-Commerce-Server"
+                    href="https://github.com/sakthiveltsmr/Pizza-back-end-using-Nodejs"
                   >
                     Backend <i className="fab fa-github"></i>
                   </a>
@@ -51,15 +51,15 @@ function Portfolio() {
                     className="bth btn-sm btn-success ml-3"
                     target="_blank"
                     rel="noreferrer"
-                    href="https://muth-ecommerce-client.netlify.app/"
+                    href="https://sakthi-pizza-beast.netlify.app/"
                   >
                     Website <i className="fad fa-link"></i>
                   </a>
                 </p>
               </div>
             </div>
-          </div> */}
-        {/* </div> */}
+          </div>
+        </div>
         <div className="card" data-aos="zoom-in-up">
           <img
             src="https://res.cloudinary.com/krishcloudstorage/image/upload/v1629096055/dev_setups/fxn61y48w5zfurvfoewa.jpg"
@@ -70,7 +70,7 @@ function Portfolio() {
             <h5 className="card-title">Blog Application</h5>
           </div>
           <div className="card-img-overlay">
-            <div className="img-title">Blogzoid</div>
+            <div className="img-title">BlogPost</div>
             <div className="img-description">
               <p>
                 {" "}
@@ -105,6 +105,48 @@ function Portfolio() {
                     target="_blank"
                     rel="noreferrer"
                     href="https://sakthi-blog-app.netlify.app/"
+                  >
+                    Website <i className="fad fa-link"></i>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card" data-aos="zoom-in-up">
+          <img src={car} className="card-img-top" alt="..." />
+          <div className="title_heading">
+            <h5 className="card-title">Car Showroom Application</h5>
+          </div>
+          <div className="card-img-overlay">
+            <div className="img-title">SKWheels</div>
+            <div className="img-description">
+              <p>
+                {" "}
+                Application system follows complete authentication for login and
+                forget password authentication Otherwise user can directly login
+                using to the google email.User can view the fully responsive
+                mobiles and systems in this application and User can see the
+                various new model cars and services.
+              </p>
+              <div className="tech_used">
+                <i className="fab fa-react react"></i> &nbsp;
+                <i className="fab fa-node node"></i> &nbsp;
+                <SiIcons.SiMongodb className="mongodb" /> &nbsp;
+                <p className="codeLinks text-center pt-3">
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/sakthiveltsmr/cars-shop"
+                  >
+                    Frontend <i className="fab fa-github"></i>
+                  </a>
+                  <a
+                    className="bth btn-sm btn-success ml-3"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://car-shop-sakthi.netlify.app/"
                   >
                     Website <i className="fad fa-link"></i>
                   </a>
